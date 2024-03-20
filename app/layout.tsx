3ab68842e8122c1e1d27174c640fb36e3316cc27
unknown <toolvlg@gmail.com>
2024-03-20 23:07:34 +0400

    adding manifest.json 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Unilever Euro 2024",
   description: "Unilever Euro 2024",
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
@@ -17,10 +17,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
